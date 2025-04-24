ef7df3e8362775232474b7c6ff784a6ded461c0b
Ddomyam <0101justin@naver.com>
2025-04-24 15:19:27 +0900

    .
    .

--- StartScene.cs
+++ StartScene.cs
@@ -20,11 +20,7 @@
             Console.WriteLine($"{player.name}님, 스파르타 마을에 오신걸 환영합니다." +
                 "\n이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
             string message = "Hello, World!";
-            foreach (char c in message)
-            {
-                Console.Write(c);
-                Thread.Sleep(100); // 하나씩 출력
-            }
+
             while (true)
             {
                 player.health = 100;
